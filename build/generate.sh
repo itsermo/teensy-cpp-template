7fa058ac3122a4301ffd6d206181750b5022abb7
Ermal Dreshaj <ermal@apple.com>
2020-05-25 20:53:32 -0700

    cleanup on build scripts

--- build/generate.sh
+++ build/generate.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-{
-    pushd "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-    cmake ../ -DCMAKE_TOOLCHAIN_FILE=../submodules/cmake-arm-embedded/toolchain-arm-none-eabi.cmake
-    popd
-} &> /dev/null
+pushd "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" &> /dev/null
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=../submodules/cmake-arm-embedded/toolchain-arm-none-eabi.cmake
+popd &> /dev/null
