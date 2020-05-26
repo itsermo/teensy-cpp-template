25ee9135e8b4ce7cbd1aa74334b187ed1ef9e01f
Ermal Dreshaj <polizeit@gmail.com>
2020-05-25 23:50:58 -0700

    fixed annoying "directory does not exist" warnings

--- build/clean.bat
+++ build/clean.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0
-    rmdir /S /Q CMakeFiles .cmake ..\bin ..\lib
-    del /Q CMake*.* cmake*.* *.map Makefile
+    rmdir /S /Q CMakeFiles .cmake ..\bin ..\lib 2>NUL
+    del /Q CMake*.* cmake*.* *.map Makefile 2>NUL
 popd
